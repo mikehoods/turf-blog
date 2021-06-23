845363ba83686ebf0d17b10319c7ee67fba16f1d
mikehoods <62457440+mikehoods@users.noreply.github.com>
2021-06-23 17:00:05 -0500

    Created base.html and started working on navbar. Setup static 'assets' directory and added some basic styling.

--- turf/settings.py
+++ turf/settings.py
@@ -108,6 +108,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "assets"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
