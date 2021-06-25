927cdc0427dc9e03a2a15f14a912d49571030588
mikehoods <62457440+mikehoods@users.noreply.github.com>
2021-06-25 18:55:59 -0500

    Added post_details, delete_post, and edit_post templates, views and urls. Styling across project.

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import HomeView, AddPostView
+from .views import HomeView, AddPostView, PostDetailView, EditPostView, DeletePostView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('add_post/', AddPostView.as_view(), name='add-post' ),
+    path('<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('<int:pk>/edit_post/', EditPostView.as_view(), name='edit-post'),
+    path('<int:pk>/delete_post/', DeletePostView.as_view(), name='delete-post'),
 
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from . models import Post
 from django.urls import reverse_lazy
 
@@ -12,4 +12,18 @@ class AddPostView(CreateView):
     model = Post
     template_name = 'add_post.html'
     fields = ('title', 'author', 'body')
+    success_url = reverse_lazy('home')
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class EditPostView(UpdateView):
+    model = Post
+    template_name = 'edit_post.html'
+    fields = ('title', 'body')
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
     success_url = reverse_lazy('home')
