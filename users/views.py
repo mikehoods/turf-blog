9770e41fa3a91591e14d7eea6ddce1f40bed3bfb
mikehoods <62457440+mikehoods@users.noreply.github.com>
2021-06-26 21:24:59 -0500

    Setup new user signup page

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView
+from .forms import SignupForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+class UserSignupView(CreateView):
+    template_name = 'registration/signup.html'
+    form_class = SignupForm
+    success_url = reverse_lazy('login')
